ccf2e15df3ee32ed4812b8af81aa1b47b977f704
unknown <harry.laza@outlook.com>
2018-07-27 14:05:28 +0200

    Create a route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ var app = express();
 
 app.use(express.static(path.join(__dirname + "././/public")));
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
