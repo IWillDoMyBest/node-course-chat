518910c70e96c85cb72bd4e61e091a875960b078
unknown <harry.laza@outlook.com>
2018-07-27 14:10:45 +0200

    Create a route

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(path.join(__dirname + "././/public")));
+app.use(express.static(path.join(__dirname + "./../public")));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
